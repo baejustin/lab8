f4e6d30b626823fb475c905fcaa84bb2921a278c
baejustin <baejustin9165@gmail.com>
2019-11-09 21:15:19 +0800

    FistYankeeUpdate

---
+++ src/lab8/Lab8SampleClass.java
@@ -0,0 +1,15 @@
+package lab8;
+
+public class Lab8SampleClass {
+	
+	public static void main(String args[])
+	{
+		Lab8SampleClass lib = new Lab8SampleClass();
+		if(lib.someLibraryMethod())
+		System.out.println("When there is a file, commit and push");
+	}
+    public boolean someLibraryMethod() {
+        return true;
+    }
+
+}
